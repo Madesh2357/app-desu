5643e480b2a970005f530c2bc15003bc7834a4d7
MADESH.S <madeshdharshan580@gmail.com>
2025-06-25 02:55:32 +0000

    I need this app in a 100% working condition, Fix all the errors, add the

--- src/components/weather-map.tsx
+++ src/components/weather-map.tsx
@@ -11,13 +11,18 @@ interface WeatherMapProps {
 const INITIAL_CENTER: L.LatLngTuple = [20.5937, 78.9629]; // Center of India
 const INITIAL_ZOOM = 5;
 
-// This component handles map events and the marker's position
-function MapEvents({ onLocationSelect }: { onLocationSelect: (lat: number, lng: number) => void }) {
-  const [position, setPosition] = useState<L.LatLng | null>(null);
+// This component handles map events like clicks and initial location
+function MapEventsHandler({ 
+  onLocationSelect, 
+  setPosition 
+}: { 
+  onLocationSelect: (lat: number, lng: number) => void;
+  setPosition: (position: L.LatLng) => void;
+}) {
   const map = useMap();
 
   useEffect(() => {
-    // Fix for default icon path issue with webpack
+    // Fix for default icon path issue with webpack and Next.js
     // @ts-ignore
     delete L.Icon.Default.prototype._getIconUrl;
     L.Icon.Default.mergeOptions({
@@ -25,16 +30,21 @@ function MapEvents({ onLocationSelect }: { onLocationSelect: (lat: number, lng: 
       iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
       shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
     });
+  }, []);
 
-    // On initial load, try to get user location
+  // On initial load, try to get user location
+  useEffect(() => {
     map.locate().on("locationfound", function (e) {
       setPosition(e.latlng);
       map.flyTo(e.latlng, 10);
       onLocationSelect(e.latlng.lat, e.latlng.lng);
     }).on("locationerror", function(e){
       console.warn("Could not fetch initial location:", e.message);
+      // If location fails, we don't do anything, user can click the map.
     });
-  }, [map, onLocationSelect]);
+  // The onLocationSelect callback is stable due to useCallback in parent,
+  // so including it is safe and correct.
+  }, [map, onLocationSelect, setPosition]);
 
   useMapEvents({
     click(e) {
@@ -44,15 +54,13 @@ function MapEvents({ onLocationSelect }: { onLocationSelect: (lat: number, lng: 
     },
   });
 
-  return position === null ? null : (
-    <Marker position={position}>
-      <Popup>Selected Location</Popup>
-    </Marker>
-  );
+  // This component does not render anything itself
+  return null;
 }
 
-
 function BaseWeatherMap({ onLocationSelect }: WeatherMapProps) {
+  const [position, setPosition] = useState<L.LatLng | null>(null);
+
   return (
     <MapContainer
       center={INITIAL_CENTER}
@@ -64,11 +72,18 @@ function BaseWeatherMap({ onLocationSelect }: WeatherMapProps) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <MapEvents onLocationSelect={onLocationSelect} />
+      <MapEventsHandler onLocationSelect={onLocationSelect} setPosition={setPosition} />
+      {position && (
+        <Marker position={position}>
+          <Popup>Selected Location</Popup>
+        </Marker>
+      )}
     </MapContainer>
   );
 }
 
+// Memoize the component to prevent re-renders when parent state changes
 const WeatherMap = memo(BaseWeatherMap);
+WeatherMap.displayName = 'WeatherMap';
 
 export default WeatherMap;
